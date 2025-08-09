74c21439dedb8c83dbbe00990478333fe9a5aa7b
RomansDigital <chassity@romansdigitaldesigns.com>
2025-08-09 02:57:52 -0400

    Commit made from VS Code Azure Static Web Apps

---
+++ # ChassityBot logo.py
@@ -0,0 +1,13 @@
+# ChassityBot logo
+logo = """
+====CHASSITY'S====
+*******~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~
+=======CODE=======
+"""
+print(logo)
+# ChassityBot is a simple bot for Discord
+# It is written in Python and uses the discord.py library
+copyright = "© 2023 ChassityBot"
+print(copyright)
